8c929f820098b9afdb18882a27fff7051150eabb
mklaso <mklasninovski@gmail.com>
2019-09-22 21:05:57 -0400

    connected database w/ server

--- face-recognition-api/server.js
+++ face-recognition-api/server.js
@@ -2,6 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
+const knex = require('knex')
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'password',
+        database: 'facerecognition'
+    }
+});
+
+db.select('*').from('users').then(data => {
+    console.log(data);
+});
 
 const app = express();
 app.use(bodyParser.json());
@@ -55,19 +70,20 @@ app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
     const saltRounds = 10;
 
-    bcrypt.hash(password, saltRounds).then(function (hash) {
-        console.log(hash);
-    })
-
-    database.users.push({
-        id: '125',
-        name: name,
-        email: email,
-        entries: 0,
-        joined: new Date()
-    })
-
-    res.json(database.users[database.users.length - 1]);
+    db('users')
+        .returning('*')
+        .insert({
+            email: email,
+            name: name,
+            joined: new Date()
+        })
+        .then(user => {
+            res.json(user[0]);
+        })
+        .catch(err => res.status(400).json('unable to register, email already exists.'))
+    // bcrypt.hash(password, saltRounds).then(function (hash) {
+    //     console.log(hash);
+    // })
 })
 
 //profile/:id
